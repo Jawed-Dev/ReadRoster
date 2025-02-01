3437407159a4d8a206dc0008f33c0ed5e4553405
jawed <jaywed@hotmail.fr>
2025-02-01 23:56:59 +0100

    Préparation côté front de la page dashboard

--- frontend/src/app/features/auth/auth-routing.module.ts
+++ frontend/src/app/features/auth/auth-routing.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
 const routes: Routes = [
   { path: 'connexion', component: LoginPageComponent },
-  { path: 'inscription', component: RegisterPageComponent }
+  { path: 'inscription', component: RegisterPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent }
 ];
 
 @NgModule({
---
+++ frontend/src/app/features/auth/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dashboard-page',
+  templateUrl: './register-dashboard.component.html',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+  ]
+})
+
+export class DashboardPageComponent {
+ 
+}
